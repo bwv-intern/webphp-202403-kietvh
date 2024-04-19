9a7cccd791b7d13ed0b8a45494113d7e5e3ca753
bwv-kiet-vh <kiet.vh@briswell-vn.com>
2024-04-19 16:22:51 +0700

    refs #127206 Coding - A-01-USE_UserList - validation form search in server

--- resources/js/screens/user/userList.js
+++ resources/js/screens/user/userList.js
@@ -13,28 +13,28 @@ $(function () {
         }
     });
 
-    // $('#formSearch').validate({
-    //     rules: {
-    //         'name': {
-    //             maxlength: 100,
-    //         },
-    //         'started_date_from': {
-    //             dateDMY: true,
-    //             startDateBeforeEndDate: true,
-    //         },
-    //         'started_date_to': {
-    //             dateDMY: true,
-    //         },
-    //     },
-    //     messages: {
-    //         'started_date_to': {
-    //             dateDMY : "Started Date Toは日付を正しく入力してください。"
-    //         },
-    //         'started_date_from': {
-    //             dateDMY : "Started Date Fromは日付を正しく入力してください。"
-    //         },
-    //     }
-    // });
+    $('#formSearch').validate({
+        rules: {
+            'name': {
+                maxlength: 100,
+            },
+            'started_date_from': {
+                dateDMY: true,
+                startDateBeforeEndDate: true,
+            },
+            'started_date_to': {
+                dateDMY: true,
+            },
+        },
+        messages: {
+            'started_date_to': {
+                dateDMY : "Started Date Toは日付を正しく入力してください。"
+            },
+            'started_date_from': {
+                dateDMY : "Started Date Fromは日付を正しく入力してください。"
+            },
+        }
+    });
 
     $("#btnClear").click(function (e) { 
         alert("Nút clear"); 
