95846ae846a5fffe4912cba24241a09f50f13caf
bwv-kiet-vh <kiet.vh@briswell-vn.com>
2024-04-23 14:11:55 +0700

    refs #127206 Coding - A-01-USE_UserList - Add seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\{DB, Hash};
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -16,14 +17,13 @@ class UserSeeder extends Seeder
         $firstNames = ['John', 'Jane', 'Michael', 'Emily', 'David', 'Sophia'];
 
         $data = [];
-        // user_flag = 1: ADMIN, 2: DIRECTOR, 3: GROUP_LEADER, 4: LEADER, 5: MEMBER
         for ($i = 1; $i <= 100; $i++) {
             $randomLastName = $lastNames[array_rand($lastNames)];
             $randomFirstName = $firstNames[array_rand($firstNames)];
 
             $randomFullName = $randomLastName . ' ' . $randomFirstName;
             $data[] = [
-                'email' => $randomFullName . $i . '@test.com',
+                'email' => Str::lower($randomLastName . $randomFirstName) . $i . '@test.com',
                 'password' => Hash::make('123456'),
                 'name' => $randomFullName,
                 'group_id' => rand(0, 10),
